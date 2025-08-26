534fd4b3f1ec37457bd6911320f19f43d17f3d6d
Danjel Peqini <106821739+danjel-peqini@users.noreply.github.com>
2025-08-27 01:39:19 +0200

    Accept userTypeIds list via POST body

--- DAL/Concrete/UserRepository.cs
+++ DAL/Concrete/UserRepository.cs
@@ -29,8 +29,15 @@ namespace DAL.Concrete
             var filterData = PaginationConfiguration(data, queryParameters.SortField, queryParameters.SortOrder, queryParameters.SearchValue);
             return PagedList<TblUser>.ToPagedList(filterData, queryParameters == null ? 1 : queryParameters.CurrentPage, queryParameters == null ? 10 : queryParameters.PageSize);
 
-        } 
- 
+        }
+
+        public PagedList<TblUser> GetUsersByTypeIds(IEnumerable<Guid> userTypeIds, QueryParameters queryParameters)
+        {
+            var data = context.Where(x => x.IsActive && userTypeIds.Contains(x.UserTypeId));
+            var filterData = PaginationConfiguration(data, queryParameters.SortField, queryParameters.SortOrder, queryParameters.SearchValue);
+            return PagedList<TblUser>.ToPagedList(filterData, queryParameters.CurrentPage, queryParameters.PageSize);
+        }
+
     }
 
 
