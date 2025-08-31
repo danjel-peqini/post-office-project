20089938f1a0325742e7521971c3da225dfa80c8
Danjel Peqini <106821739+danjel-peqini@users.noreply.github.com>
2025-08-31 16:43:54 +0200

    Link groups to programs

--- Entities/Models/TblProgram.cs
+++ Entities/Models/TblProgram.cs
@@ -11,6 +11,7 @@ namespace Entities.Models
             TblCourses = new HashSet<TblCourse>();
             TblGroups = new HashSet<TblGroup>();
             TblStudentCards = new HashSet<TblStudentCard>();
+            TblSchedules = new HashSet<TblSchedule>();
         }
 
         public Guid Id { get; set; }
@@ -22,5 +23,6 @@ namespace Entities.Models
         public virtual ICollection<TblCourse> TblCourses { get; set; }
         public virtual ICollection<TblGroup> TblGroups { get; set; }
         public virtual ICollection<TblStudentCard> TblStudentCards { get; set; }
+        public virtual ICollection<TblSchedule> TblSchedules { get; set; }
     }
 }
